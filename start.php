323cb01257b6443389dd631e3e37a804ac0b0bf5
Jonathan Baldie <jon@jonbaldie.com>
2022-02-04 07:55:38 +0000

    use the correct spelling here

--- start.php
+++ start.php
@@ -8,15 +8,15 @@ $api = new App('http://0.0.0.0:3000');
 
 $api->count = 4; // process count
 
-$api->any('/', function ($requst) {
+$api->any('/', function ($request) {
     return 'Hello world';
 });
 
-$api->get('/hello/{name}', function ($requst, $name) {
+$api->get('/hello/{name}', function ($request, $name) {
     return "Hello $name";
 });
 
-$api->post('/user/create', function ($requst) {
+$api->post('/user/create', function ($request) {
     return json_encode(['code' => 200 ,'message' => 'OKAY'], JSON_PRETTY_PRINT);
 });
 
